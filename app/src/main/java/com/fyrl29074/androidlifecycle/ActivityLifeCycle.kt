23ed08d79a0aea4c6353b1870d4984d627d89f7b
Olegk <Kimjk1313@gmail.com>
2022-05-16 20:45:58 +0300

    create activity and fragment

---
+++ app/src/main/java/com/fyrl29074/androidlifecycle/ActivityLifeCycle.kt
@@ -0,0 +1,11 @@
+package com.fyrl29074.androidlifecycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityLifeCycle : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_life_cycle)
+    }
+}
--- app/src/main/java/com/fyrl29074/androidlifecycle/MainActivity.kt
+++ app/src/main/java/com/fyrl29074/androidlifecycle/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.fyrl29074.androidlifecycle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun setListeners() {
+        findViewById<Button>(R.id.activity).setOnClickListener {
+            val intent = Intent(this, ActivityLifeCycle::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.fragment).setOnClickListener {
+
+        }
     }
 }
